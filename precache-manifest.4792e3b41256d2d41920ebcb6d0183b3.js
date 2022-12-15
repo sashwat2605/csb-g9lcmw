adc85667b2fc70b2e8f29f1411c8bf69a7d890e5
Code Sandbox <hello@codesandbox.io>
2022-12-15 06:23:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4792e3b41256d2d41920ebcb6d0183b3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2338ab964d40072fe81e1dcb2a2b839a",
+    "url": "https://sashwat2605.github.io/csb-g9lcmw/index.html"
+  },
+  {
+    "revision": "60424039a171866cb3d4",
+    "url": "https://sashwat2605.github.io/csb-g9lcmw/static/js/2.801ad9b3.chunk.js"
+  },
+  {
+    "revision": "3cf22c64dd8c80e5094f",
+    "url": "https://sashwat2605.github.io/csb-g9lcmw/static/js/main.151625e3.chunk.js"
+  },
+  {
+    "revision": "d5d516030306f1765450",
+    "url": "https://sashwat2605.github.io/csb-g9lcmw/static/js/runtime-main.ab6cc361.js"
+  }
+]);
